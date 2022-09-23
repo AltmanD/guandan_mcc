ea155170c259eecce01a691cea361531b60330bd
AltmanD <luyu.dong@foxmail.com>
2022-09-23 20:53:20 +0800

    Add waiting image before start play

--- showdown/showdown_actor.py
+++ showdown/showdown_actor.py
@@ -9,29 +9,66 @@ from pyarrow import deserialize, serialize
 class Game:
     def __init__(self) -> None:
         pygame.init()
+        waiting_image_path = 'showdown/images/waiting.png'
+        screen = pygame.display.set_mode((900, 600), 0, 32)
+        waitingImage = pygame.image.load(waiting_image_path).convert()
+        screen.blit(waitingImage, (0, 0))  # 画等待
+        pygame.display.update()
+    
+    def initInfo(self, message):
+        self.my_handCards = message['handCards']
+        self.curRank = message['curRank']
+        self.selfRank = message['selfRank']
+        self.oppRank = message['oppoRank']
+
+    def showhandCards(self):
         pass
 
-    def update(self, message):
+    def showpublicInfo(self):
         pass
 
+    def updateInfo(self, message: dict):
+        self.my_handCards = message['handCards']
+        self.showhandCards()
+        self.publicInfo = message['publicInfo']
+        self.showpublicInfo()
+        self.curRank = message['curRank']
+        self.selfRank = message['selfRank']
+        self.oppRank = message['oppoRank']
+
+        self.curPos = message['curPos']
+        self.curAction = message['curAction']
+        self.greaterPos = message['greaterPos']
+        self.greaterAction = message['greaterAction']
+
+        self.legalActions = message['actionList']
+        
     def waitInput(self):
-        pass
+        return 0
 
 
 def run_one_player():
     board = Game()
 
-    # 初始化zmq
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind(f'tcp://*:{6003}')
+    # # 初始化zmq
+    # context = zmq.Context()
+    # socket = context.socket(zmq.REP)
+    # socket.bind(f'tcp://*:{6003}')
 
     action_index = 0
-    while True:
-        message = deserialize(socket.recv())
-        board.update(message)
-        action_index = board.waitInput()
-        socket.send(serialize(action_index).to_buffer())
+    file = open('showdown/test/test_message.log', 'r')
+    messages = file.readlines()
+    for message in messages:
+        message = eval(message)
+        if message['type'] == 'notify':
+            if message['stage'] == 'beginning':
+                board.initInfo(message)
+        elif message['type'] == 'act':
+            board.updateInfo(message)
+            action_index = board.waitInput()
+        # message = deserialize(socket.recv())
+        print('choose action ', action_index, 'in this round')
+        # socket.send(serialize(action_index).to_buffer())
 
 
 def main():
@@ -45,7 +82,6 @@ def main():
 
     players = []
     for i in [1]:
-        print(f'start{i}')
         p = Process(target=exit_wrapper)
         p.start()
         time.sleep(0.5)
---
+++ showdown/test.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+pygame.init()
+
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("showdown/images/waiting.png")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
