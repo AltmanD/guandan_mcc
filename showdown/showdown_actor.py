ebddfb90c69d77e2f2c0dc0f3c158837ffec9c19
AltmanD <luyu.dong@foxmail.com>
2022-09-27 17:05:18 +0800

    Add handCards showdown

--- showdown/showdown_actor.py
+++ showdown/showdown_actor.py
@@ -9,12 +9,19 @@ from pyarrow import deserialize, serialize
 class Game:
     def __init__(self) -> None:
         pygame.init()
+        self.font = pygame.font.Font('showdown/fonts/HanYiRegular.ttf', 40)
         self.cards_image_path = 'showdown/images/cards/'
+        self.background_image_path = 'showdown/images/background.png'
+        self.backgroundImage = pygame.image.load(self.background_image_path)
+        self.cardbackImage = pygame.image.load(self.cards_image_path + 'back.bmp')
+        self.height = 900
+        self.width = 1200
 
         waiting_image_path = 'showdown/images/waiting.png'
-        self.screen = pygame.display.set_mode((900, 600), 0, 32)
+        self.screen = pygame.display.set_mode((self.width, self.height), 0, 32)
         waitingImage = pygame.image.load(waiting_image_path).convert()
-        self.screen.blit(waitingImage, (0, 0))  # 画等待
+        waitingImage_resize = pygame.transform.scale(waitingImage, (self.width, self.height))
+        self.screen.blit(waitingImage_resize, (0, 0))  # 画等待
         pygame.display.update()
     
     def initInfo(self, message):
@@ -23,23 +30,28 @@ class Game:
         self.selfRank = message['selfRank']
         self.oppRank = message['oppoRank']
 
-    def showhandCards(self):
-        print(self.my_handCards)
-        
-        for poker2 in a3:
-            if poker2 == '':
-
-                break
+    def showhandCards(self):    # 显示我方手牌
+        self.screen.blit(pygame.transform.scale(self.backgroundImage, (self.width, self.height)), (0, 0))
+        self.screen.blit(self.cardbackImage, (40, self.height/2-100))
+        self.screen.blit(self.cardbackImage, (self.width-100, self.height/2-100))
+        self.screen.blit(self.cardbackImage, (self.width/2-50, 30))
+        base_width = (self.width - len(self.my_handCards) * 30)/2 - 50
+        for i, poker2 in enumerate(self.my_handCards):
             poker_outhand = pygame.image.load(
                 self.cards_image_path + poker2 + '.jpg').convert_alpha()
-            self.screen.blit(poker_outhand, (260 + x1, 430))
-            x1 += 20
-            # sleep(0.01)
+            self.screen.blit(poker_outhand, (base_width + i*30, self.height-200))
         pygame.display.update()
-        pass
 
     def showpublicInfo(self):
-        pass
+        print(self.publicInfo)
+        self.myInfo = self.publicInfo[0]
+        self.nexInfo = self.publicInfo[1]
+        self.oppInfo = self.publicInfo[2]
+        self.preInfo = self.publicInfo[3]
+        
+        self.screen.blit(self.font.render("按下Q退出", True, (255, 255, 0)), (self.width/2, self.height/2))
+        
+        pygame.display.update()
 
     def updateInfo(self, message: dict):
         self.my_handCards = message['handCards']
