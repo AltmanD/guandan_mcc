8cefdd8fbd9362acbe8da00774f0d00b37a3eb25
AltmanD <luyu.dong@foxmail.com>
2022-10-14 15:28:38 +0800

    Finish showdown function

--- showdown/showdown_actor.py
+++ showdown/showdown_actor.py
@@ -4,15 +4,22 @@ from multiprocessing import Process
 import pygame
 import zmq
 from pyarrow import deserialize, serialize
+from pygame.locals import *
 
 
 class Game:
     def __init__(self) -> None:
         pygame.init()
-        self.font = pygame.font.Font('showdown/fonts/HanYiRegular.ttf', 40)
+        self.font = pygame.font.Font('showdown/fonts/HanYiRegular.ttf', 20)
         self.cards_image_path = 'showdown/images/cards/'
         self.background_image_path = 'showdown/images/background.png'
+        self.partbackground_image_path = 'showdown/images/partback.bmp'
+        self.chup_image_path = 'showdown/images/chup.png'
+        self.buc_image_path = 'showdown/images/buc.png'
+        self.chupImage = pygame.image.load(self.chup_image_path)
+        self.bucImage = pygame.image.load(self.buc_image_path)
         self.backgroundImage = pygame.image.load(self.background_image_path)
+        self.partback = pygame.image.load(self.partbackground_image_path)
         self.cardbackImage = pygame.image.load(self.cards_image_path + 'back.bmp')
         self.height = 900
         self.width = 1200
@@ -20,8 +27,7 @@ class Game:
         waiting_image_path = 'showdown/images/waiting.png'
         self.screen = pygame.display.set_mode((self.width, self.height), 0, 32)
         waitingImage = pygame.image.load(waiting_image_path).convert()
-        waitingImage_resize = pygame.transform.scale(waitingImage, (self.width, self.height))
-        self.screen.blit(waitingImage_resize, (0, 0))  # 画等待
+        self.screen.blit(pygame.transform.scale(waitingImage, (self.width, self.height)), (0, 0))  # 画等待
         pygame.display.update()
     
     def initInfo(self, message):
@@ -33,24 +39,29 @@ class Game:
     def showhandCards(self):    # 显示我方手牌
         self.screen.blit(pygame.transform.scale(self.backgroundImage, (self.width, self.height)), (0, 0))
         self.screen.blit(self.cardbackImage, (40, self.height/2-100))
-        self.screen.blit(self.cardbackImage, (self.width-100, self.height/2-100))
+        self.screen.blit(self.cardbackImage, (self.width-120, self.height/2-100))
         self.screen.blit(self.cardbackImage, (self.width/2-50, 30))
-        base_width = (self.width - len(self.my_handCards) * 30)/2 - 50
-        for i, poker2 in enumerate(self.my_handCards):
-            poker_outhand = pygame.image.load(
-                self.cards_image_path + poker2 + '.jpg').convert_alpha()
-            self.screen.blit(poker_outhand, (base_width + i*30, self.height-200))
+        self.base_width = (self.width - len(self.my_handCards) * 30)/2 - 50
+
+        self.pokerImages = []
+        for i, poker in enumerate(self.my_handCards):
+            pokerImage = pygame.image.load(self.cards_image_path + poker + '.jpg').convert_alpha()    # (105,150)
+            self.pokerImages.append(pokerImage)
+            self.screen.blit(pokerImage, (self.base_width + i*30, self.height-200))
         pygame.display.update()
 
     def showpublicInfo(self):
         print(self.publicInfo)
-        self.myInfo = self.publicInfo[0]
-        self.nexInfo = self.publicInfo[1]
-        self.oppInfo = self.publicInfo[2]
-        self.preInfo = self.publicInfo[3]
-        
-        self.screen.blit(self.font.render("按下Q退出", True, (255, 255, 0)), (self.width/2, self.height/2))
-        
+        texts_positions = [(self.width/2, self.height-50), (self.width-120, self.height/2+30), (self.width/2-50, 160), (40, self.height/2+30)]
+        cards_positions = [(self.width/2-100, self.height-500), (self.width-350, self.height/2-100), (self.width/2-100, 200), (150, self.height/2-100)]
+        for i, (info, tpos, cpos) in enumerate(zip(self.publicInfo, texts_positions, cards_positions)):
+            if i != 0:
+                self.screen.blit(self.font.render(f"剩余{info['rest']}张牌", True, (0,0,0)), tpos)
+            if info['playArea'] != None and info['playArea'][0] != None and info['playArea'][0] != 'PASS':
+                for j, poker in enumerate(info['playArea'][2]):
+                    if poker[0] != None:
+                        pokerImage = pygame.image.load(self.cards_image_path + poker + '.jpg').convert_alpha()
+                        self.screen.blit(pokerImage, (cpos[0] + j*30, cpos[1]))
         pygame.display.update()
 
     def updateInfo(self, message: dict):
@@ -68,18 +79,109 @@ class Game:
         self.greaterAction = message['greaterAction']
 
         self.legalActions = message['actionList']
+        self.legalActions_set = []
+        for action in self.legalActions[1:]:
+            self.legalActions_set.append(set(action[2]))
         
     def waitInput(self):
-        return 0
+        cards_up = []
+        cards_down = list(range(len(self.pokerImages)))
+        status_flag = 0
+        action_choose = None
+        action_index = None
+        if len(self.legalActions) == 1:
+            self.screen.blit(pygame.transform.scale(self.bucImage, (90, 40)), (self.width/2-30, self.height - 345))
+            status_flag = 1
+        elif ['PASS', 'PASS', 'PASS'] not in self.legalActions:
+            self.screen.blit(pygame.transform.scale(self.chupImage, (90, 40)), (self.width/2-30, self.height - 345))
+            status_flag = 2
+        else:
+            self.screen.blit(pygame.transform.scale(self.chupImage, (90, 40)), (self.width*2/3-30, self.height - 345))
+            self.screen.blit(pygame.transform.scale(self.bucImage, (90, 40)), (self.width/3-30, self.height - 345))
+            status_flag = 3
+        pygame.display.update()
+
+        while action_index is None:
+            for event in pygame.event.get():
+                print(event)
+                if event.type == QUIT:
+                    exit()
+                if event.type == MOUSEBUTTONDOWN:
+                    x, y = pygame.mouse.get_pos()
+                elif event.type == MOUSEBUTTONUP:
+                    # 点击扑克牌
+                    print(x, y)
+                    self.screen.blit(pygame.transform.scale(self.partback, (self.width, self.height/3)), (0, self.height*2/3))
+                    if self.height-230 <= y <= self.height-50 and self.base_width <= x <= self.base_width+len(self.pokerImages)*30+75:
+                        if self.base_width+len(self.pokerImages)*30+75 - x < 105:
+                            card_index = len(self.pokerImages)-1
+                        else:
+                            card_index = int((x - self.base_width) // 30)
+                        if card_index in cards_down and card_index not in cards_up:
+                            cards_down.remove(card_index)
+                            cards_up.append(card_index)
+                        elif card_index in cards_up and card_index not in cards_down:
+                            cards_up.remove(card_index)
+                            cards_down.append(card_index)
+                    elif self.height - 345 <= y <= self.height - 305:
+                        if status_flag == 1 and self.width/2-30 <= x <= self.width/2+60:
+                            action_choose = ['PASS']
+                            action_index = 0
+                        elif status_flag == 2 and self.width/2-30 <= x <= self.width/2+60:
+                            action_choose = []
+                            for index in cards_up:
+                                action_choose.append(self.my_handCards[index])
+                        elif status_flag == 3:
+                            if self.width/3-30 <= x <= self.width/3+60:
+                                action_choose = ['PASS']
+                                action_index = 0
+                                cards_down.extend(cards_up)
+                                cards_up = []
+                            elif self.width*2/3-30 <= x <= self.width*2/3+60:
+                                action_choose = []
+                                for index in cards_up:
+                                    action_choose.append(self.my_handCards[index])
+                    print(action_choose)
+                    if action_choose is not None and action_index is None:
+                        if set(action_choose) in self.legalActions_set:
+                            action_index = self.legalActions_set.index(set(action_choose))
+                            cards_up = []
+                        else:
+                            cards_down.extend(cards_up)
+                            cards_up = []
+                            self.screen.blit(self.font.render(f"刚刚出的牌不符合规则", True, (0,0,0)), (self.width/2-90, self.height - 300))
+                            action_choose = None
+
+                    for i, card in enumerate(self.pokerImages):
+                        if i in cards_up:
+                            self.screen.blit(card, (self.base_width + i*30, self.height-230))
+                        else:
+                            self.screen.blit(card, (self.base_width + i*30, self.height-200))
+                    pygame.display.update()
+                if action_index is not None:
+                    break
+        return action_index
 
 
 def run_one_player():
     board = Game()
 
-    # # 初始化zmq
-    # context = zmq.Context()
-    # socket = context.socket(zmq.REP)
-    # socket.bind(f'tcp://*:{6003}')
+    # 初始化zmq
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind(f'tcp://*:{6003}')
+    action_index = 0
+    while True:
+        message = deserialize(socket.recv())
+        message = eval(message)
+        if message['type'] == 'notify':
+            if message['stage'] == 'beginning':
+                board.initInfo(message)
+        elif message['type'] == 'act':
+            board.updateInfo(message)
+            action_index = board.waitInput()
+        print('choose action ', action_index, 'in this round')
+        socket.send(serialize(action_index).to_buffer())
 
     action_index = 0
     file = open('showdown/test/test_message.log', 'r')
--- showdown/test.py
+++ showdown/test.py
@@ -2,27 +2,32 @@ import sys
 
 import pygame
 
-pygame.init()
+# pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+# size = width, height = 320, 240
+# speed = [2, 2]
+# black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+# screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("showdown/images/waiting.png")
-ballrect = ball.get_rect()
+# ball = pygame.image.load("showdown/images/waiting.png")
+# ballrect = ball.get_rect()
 
-while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+# while 1:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+#     ballrect = ballrect.move(speed)
+#     if ballrect.left < 0 or ballrect.right > width:
+#         speed[0] = -speed[0]
+#     if ballrect.top < 0 or ballrect.bottom > height:
+#         speed[1] = -speed[1]
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+#     screen.fill(black)
+#     screen.blit(ball, ballrect)
+#     pygame.display.flip()
+
+a = set(['PASS'])
+b = set([2,1,3,2])
+print(a,b)
+print(a==b)
